0b277c829f8f39e8e917f963df58f348ff99b5a8
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-03-13 12:35:45 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Jonathan Wheeler Jr. on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
